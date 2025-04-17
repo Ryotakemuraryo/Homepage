b9ad495bd36550efe3f1a6771e018a2babead793
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-04-17 14:46:07 -0700

    update

--- global.js
+++ global.js
@@ -39,8 +39,11 @@ for (let p of pages) {
     if (!url.startsWith('http')) {
         url = BASE_PATH + url;
     }
-    
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
 }
 
 
