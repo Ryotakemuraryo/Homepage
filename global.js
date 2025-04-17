81ae36264295110e39fe88cf133f57c078b6ddcf
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-04-17 13:45:29 -0700

    debug

--- global.js
+++ global.js
@@ -15,6 +15,11 @@ function $$(selector, context = document) {
 //     currentLink.classList.add('current');// add current to class so .clss in CSS will be adapted
 // }
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server(when location.hostname is local host or 127.0.0.1)
+  : "/Lab0/";         // GitHub Pages repo name(otherwise)
+
+
 let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
@@ -31,6 +36,11 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // next step: create link and add it to nav
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    }
+    
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
+
 
