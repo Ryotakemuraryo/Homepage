f5a1d8a78f135f8bdbcc06bdedae62e81fe28e88
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-04-17 13:19:31 -0700

    nav menu by JS

--- global.js
+++ global.js
@@ -4,13 +4,33 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a")
+// const navLinks = $$("nav a")
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-if (currentLink) {
-    // or if (currentLink !== undefined)
-    currentLink.classList.add('current');// add current to class so .clss in CSS will be adapted
+// if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');// add current to class so .clss in CSS will be adapted
+// }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/Ryotakemuraryo', title: 'GitHub'},
+    { url: 'cv.html', title: 'CV'}
+    // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
+
