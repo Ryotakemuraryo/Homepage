0df5359c09417a2109232bc40e332e9b2ea5379e
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-04-24 15:31:57 -0700

    update number of article

--- projects/projects.js
+++ projects/projects.js
@@ -6,4 +6,9 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const title_element = document.querySelector('.projects-title');
+if (title_element && projects) {
+    title_element.textContent = `${projects.length} Projects`;
+}
 })();
