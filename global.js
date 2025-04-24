3ad847850d21bb81d762c95bf9d2228c6578c4d9
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-04-24 15:11:08 -0700

    update js

--- global.js
+++ global.js
@@ -105,9 +105,25 @@ export async function fetchJSON(url) {
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
 window.fetchJSON = fetchJSON;
 
+
+export function renderProjects(project, containerElement, headingLevel='h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';//to clear the existing content of the container element.
+  for (let p of project) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <${headingLevel}>${p.title}</${headingLevel}>
+    <img src="${p.image}" alt="${p.title}">
+    <p>${p.description}</p>`;
+    containerElement.appendChild(article);
+}
+  }
+  
