56dc64929d633370a73c3c26896b695c2d2ab937
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-04-24 22:37:34 -0700

    debug

--- global.js
+++ global.js
@@ -127,3 +127,10 @@ export function renderProjects(project, containerElement, headingLevel='h2') {
 }
 }
   
+
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+  
+}
