2ee80733e47c1ef2adee7668d7af4a8440bdfbc1
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-02 10:46:35 -0700

    add value

--- projects/projects.js
+++ projects/projects.js
@@ -33,12 +33,12 @@ for (let d of data) {
 }
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let arcs = arcData.map((d) => arcGenerator(d)); */
-let data = [1, 2];
+let data = [1, 2, 3, 4, 5, 5];
 let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
-let colors = ['gold', 'purple'];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 arcs.forEach((arc, idx) => {
     // TODO, fill in step for appending path to svg using D3
-    d3.select('svg').append('path').attr('d', arc).attr('fill', colors[idx])
+    d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx))
   });
