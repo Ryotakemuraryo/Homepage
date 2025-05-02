8ab2f3d8a58a6d15e8f6274309c48e392f439bbb
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-02 10:24:42 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -20,3 +20,26 @@ let arc = d3.arc().innerRadius(0).outerRadius(50)({
   });
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let data = [1, 2];
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc) => {
+    // TODO, fill in step for appending path to svg using D3
+    d3.select('svg').append()
+  });
