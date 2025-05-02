994305285e0ef0b2710951f72cf0ee905d06abe5
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-01 22:19:20 -0700

    change by usnig d3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 (async () => {
 const projects = await fetchJSON('../lib/projects.json');
@@ -12,3 +13,10 @@ if (title_element && projects) {
     title_element.textContent = `${projects.length} Projects`;
 }
 })();
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
