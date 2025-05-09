929089744c0165c8414059211d3631a057198a4a
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-09 14:24:36 -0700

    add meta data shown

--- meta/main.js
+++ meta/main.js
@@ -43,6 +43,47 @@ function processCommits(data) {
       });
   }
   
+
+
+
+function renderCommitInfo(data, commits) {
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+    const fileLengths = d3.rollups(
+      data,
+      (v) => d3.max(v, (v) => v.line),
+      (d) => d.file,
+    );
+    const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+
+    const workByPeriod = d3.rollups(
+      data,
+      (v) => v.length,
+      (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
+    );
+    const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+  
+    // Add total commits
+    dl.append('dt').text('Total commits');
+    dl.append('dd').text(commits.length);
+  
+    // Add more stats as needed...
+    dl.append('dt').text('Total files');
+    dl.append('dd').text(totalfiles);
+
+    dl.append('dt').text('Average file lengths');
+    dl.append('dd').text(Math.round(averageFileLength));
+
+    dl.append('dt').text('Most working time');
+    dl.append('dd').text(maxPeriod);
+  }
+  
   let data = await loadData();
   let commits = processCommits(data);
-  console.log(commits);
+  let totalfiles = d3.group(data, d => d.file).size;
+  
+  renderCommitInfo(data, commits);
