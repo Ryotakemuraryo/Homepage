1b3d805c138531bc33088a17290ad1602056e59a
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-09 15:18:38 -0700

    Step2

--- meta/main.js
+++ meta/main.js
@@ -63,6 +63,8 @@ function renderCommitInfo(data, commits) {
       (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
     );
     const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+
+    let totalfiles = d3.group(data, d => d.file).size;
     // Add total LOC
     dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
     dl.append('dd').text(data.length);
@@ -80,10 +82,90 @@ function renderCommitInfo(data, commits) {
 
     dl.append('dt').text('Most working time');
     dl.append('dd').text(maxPeriod);
+
+    
   }
   
-  let data = await loadData();
-  let commits = processCommits(data);
-  let totalfiles = d3.group(data, d => d.file).size;
+
+function renderScatterPlot(data, commits) {
+    // Put all the JS code of Steps inside this function
+   const width = 1000;
+   const height = 600;
+   const svg = d3
+  .select('#chart')
+  .append('svg')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+  .style('overflow', 'visible');
+  const xScale = d3
+  .scaleTime()
+  .domain(d3.extent(commits, (d) => d.datetime))
+  .range([0, width])
+  .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+   
+
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
   
-  renderCommitInfo(data, commits);
+    // Update scales with new ranges
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+
+    // Add gridlines BEFORE the axes
+  const gridlines = svg
+  .append('g')
+  .attr('class', 'gridlines')
+  .attr('transform', `translate(${usableArea.left}, 0)`);
+
+  // Create gridlines as an axis with no labels and full-width ticks
+  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+    // Create the axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');  
+
+
+  // Add X axis
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  // Add Y axis
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);    
+    const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+      .selectAll('circle')
+      .data(commits)
+      .join('circle')
+      .attr('cx', (d) => xScale(d.datetime))
+      .attr('cy', (d) => yScale(d.hourFrac))
+      .attr('r', 5)
+      .attr('fill', 'steelblue'); 
+    }
+    
+  async function main() {
+      const data = await loadData();         
+      const commits = processCommits(data); 
+    
+      renderCommitInfo(data, commits);    
+      renderScatterPlot(data, commits);            
+  }
+  
+  main(); // 実行スタート
+  
