c4c405652d4af7bb8bb6d403a22fe92dd7a89217
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-09 18:06:40 -0700

    step3

--- meta/main.js
+++ meta/main.js
@@ -150,22 +150,58 @@ function renderScatterPlot(data, commits) {
     const dots = svg.append('g').attr('class', 'dots');
 
   dots
-      .selectAll('circle')
-      .data(commits)
-      .join('circle')
-      .attr('cx', (d) => xScale(d.datetime))
-      .attr('cy', (d) => yScale(d.hourFrac))
-      .attr('r', 5)
-      .attr('fill', 'steelblue'); 
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+    .on('mouseenter', (event, commit) => {
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on('mouseleave', () => {
+      // TODO: Hide the tooltip
+      updateTooltipVisibility(false);
+    });
     }
     
-  async function main() {
+
+function renderTooltipContent(commit) {
+      const link = document.getElementById('commit-link');
+      const date = document.getElementById('commit-date');
+    
+      if (Object.keys(commit).length === 0) return;
+    
+      link.href = commit.url;
+      link.textContent = commit.id;
+      date.textContent = commit.datetime?.toLocaleString('en', {
+        dateStyle: 'full',
+      });
+    }
+
+function updateTooltipVisibility(isVisible) {
+      const tooltip = document.getElementById('commit-tooltip');
+      tooltip.hidden = !isVisible;
+    }
+
+
+function updateTooltipPosition(event) {
+      const tooltip = document.getElementById('commit-tooltip');
+      tooltip.style.left = `${event.clientX}px`;
+      tooltip.style.top = `${event.clientY}px`;
+    }
+
+    
+async function main() {
       const data = await loadData();         
       const commits = processCommits(data); 
     
       renderCommitInfo(data, commits);    
-      renderScatterPlot(data, commits);            
+      renderScatterPlot(data, commits);       
   }
   
-  main(); // 実行スタート
+main(); // 実行スタート
   
